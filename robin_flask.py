47324190b40794f661313aef907b41f68db2e7a0
Anteneh M <amoges1@gmail.com>
2019-01-06 10:21:38 -0500

    added comments, sessions, logout

--- robin_flask.py
+++ robin_flask.py
@@ -2,46 +2,63 @@
 
 import flask
 import robinsquad
-
 app = flask.Flask(__name__) # a Flask object
+app.secret_key = 'gNMsBGT8xPxJkUl2VeODQnFf6bSUuxZcV;pN$m6!vE<]d*;' #session
 
 @app.route('/')
 def welcome():
+    # redirect based on session
+    if 'email' in flask.session:
+        return flask.redirect(flask.url_for("home"))    
     return flask.render_template('welcome.html')
 
 @app.route('/home', methods = ['POST', 'GET'])
 def home():
+    # return/update Robinhood account in session
+    if 'email' in flask.session:
+        email, password = flask.session['email'], flask.session['password']
+        robinsquad.get_acc_info(email, password)
+        return flask.render_template('home.html', email=email)
 
-    # No creds, redirect to home
-    if flask.request.method == "GET":
-        return flask.redirect(flask.url_for("/"))
+     # no session, redirect to welcome
+    if flask.request.method == 'GET':
+        # note: url_for params: function, not url
+        return flask.redirect(flask.url_for("welcome")) 
     
-    # Retrieve credentials and log into Robinhood 
+    # retrieve creds and log into Robinhood 
     email = flask.request.form.get('email')
     password = flask.request.form.get('password')
+    success =  robinsquad.get_acc_info(email, password)
 
-    # log into Robinhood
-    success =  robinsquad.get_user_info(email, password)
-
-    # Bad creds, redirect to welcome
+    # bad creds, redirect to welcome
     if not success:
-        return flask.redirect(flask.url_for("/"))
+        return flask.redirect(flask.url_for("welcome"))
+    
+    # set session for sucessful login
+    flask.session['email'] = email
+    flask.session['password'] = password
     
     return flask.render_template('home.html', email=email)
 
 @app.route('/account', methods=['GET'])
 def account():
+    # return account info
     response = flask.send_file('acc.json')
     response.headers.add("Access-Control-Allow-Origin", "*")
-    return response 
+    return response
 
 @app.route('/invest', methods=['GET'])
 def invest():
-
+    # display new purchases for user
     purchases, email = robinsquad.rebalance()
-
     return flask.render_template('results.html', purchases = purchases, email = email)
-    
+
+@app.route('/logout')
+def logout():
+    # clear session for next user
+    flask.session.clear()
+    return flask.redirect(flask.url_for("welcome"))
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
