d595d2d93b9e1fca0815a2e19286f8453da3a0ed
amoges1 <am4970@nyu.edu>
2020-09-02 20:45:43 -0400

    redeveloped into MongoDB, Flask, React app

--- robin_flask.py
+++ robin_flask.py
@@ -1,64 +1,59 @@
 #!/usr/bin/env python
 
 import flask
+from flask_cors import CORS, cross_origin
 import robinsquad
 app = flask.Flask(__name__) # a Flask object
 app.secret_key = 'gNMsBGT8xPxJkUl2VeODQnFf6bSUuxZcV;pN$m6!vE<]d*;' #session
+CORS(app)
 
-@app.route('/')
-def welcome():
-    # redirect based on session
-    if 'email' in flask.session:
-        return flask.redirect(flask.url_for("home"))    
-    return flask.render_template('welcome.html')
 
 @app.route('/home', methods = ['POST', 'GET'])
 def home():
-    # return/update Robinhood account in session
-    if 'email' in flask.session:
-        email, password = flask.session['email'], flask.session['password']
-        robinsquad.get_acc_info(email, password)
-        return flask.render_template('home.html', email=email)
-
-     # no session, redirect to welcome
-    if flask.request.method == 'GET':
-        # note: url_for params: function, not url
-        return flask.redirect(flask.url_for("welcome")) 
     
     # retrieve creds and log into Robinhood 
-    email = flask.request.form.get('email')
-    password = flask.request.form.get('password')
-    success =  robinsquad.get_acc_info(email, password)
+    email = flask.request.form.get("email")
+    password = flask.request.form.get("password")
+    success = robinsquad.login(email, password)
 
     # bad creds, redirect to welcome
     if not success:
-        return flask.redirect(flask.url_for("welcome"))
+        err = {
+            "msg": "Bad credentials"
+        }
+        return flask.jsonify(err) 
     
-    # set session for sucessful login
-    flask.session['email'] = email
-    flask.session['password'] = password
-    
-    return flask.render_template('home.html', email=email)
+    err = {
+        "msg": "Signed in"
+    }
+    return flask.jsonify(err) 
 
 @app.route('/account', methods=['GET'])
 def account():
     # return account info
-    response = flask.send_file('acc.json')
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+    email = flask.request.args.get("email")
+    data = robinsquad.get_acc_info(email)
+    return flask.jsonify(data)
+     
 
 @app.route('/invest', methods=['GET'])
 def invest():
     # display new purchases for user
-    purchases, email = robinsquad.rebalance()
-    return flask.render_template('results.html', purchases = purchases, email = email)
+    email = flask.request.args.get("email")
+    password = flask.request.args.get("password")
+    max_amt = int(flask.request.args.get("max_amt"))
+    purchases = robinsquad.rebalance(email, password, max_amt)
+    return flask.jsonify(purchases)
 
 @app.route('/logout')
 def logout():
     # clear session for next user
-    flask.session.clear()
-    return flask.redirect(flask.url_for("welcome"))
-
+    email = flask.request.args.get("email")
+    robinsquad.logout(email)
+    err = {
+        "msg": "Success logout"
+    }
+    return flask.jsonify(err)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- robinsquad.py
+++ robinsquad.py
@@ -1,135 +1,119 @@
-from Robinhood import Robinhood
+import flask_vars
+import robin_stocks as r
+import pymongo
 import pprint as pprint
 import json
 import ast
 
-class Stock:
-    '''Stock holds symbol, name, quantity, value'''
-    def __init__(self, symbol, name, quantity, value) :
-        self.symbol = symbol
-        self.name = name
-        self.quantity = quantity
-        self.total_value = value
-        self.percentage = 0.0
+# import pymongo (needs dnspython)
+# connect to MongoDB
+client = pymongo.MongoClient(flask_vars.mongoDB)
+db = client.robinsquad
 
-def get_port_info(my_acc, instrs_quantity):
+def get_port_info(email): # my_acc, instrs_quantity
     '''Retrieve a list of stock objects from portfolio'''
-    port_stocks, port_equity = [], 0
+    return db.users.find_one({"email": email})["portfolio"]
 
-    for instr, quantity in instrs_quantity.items():
-        stock = my_acc.instrument(instr)
-        symbol, name = stock['symbol'], stock['simple_name']
-        # calculate stock's equity in dollars
-        stock_equity = float(quantity) * float(my_acc.quote_data(symbol)['adjusted_previous_close'])
-        stock_obj = Stock(symbol, name, int(float(quantity)), round(stock_equity,2))
-        
-        port_stocks.append(stock_obj)
-        port_equity += int(float(quantity))
-    
-    # calculate stock's percentage
-    for stock in port_stocks:
-        stock.percentage = round( (stock.quantity / port_equity) , 4)
-
-    return port_stocks 
-
-def get_acc_info(email, password):
-    '''Retrieve user account information e.g equity, stocks, etc. and upload to json'''
+def login(email, password):
+    '''Retrieve user account information e.g email, portfolio, equity, cash'''
     # log into Robinhood account
-    my_acc = Robinhood()
-    try:
-        my_acc.login(email,password)
-    except:
-        return False
+    r.authentication.login(email, password)
 
-    # from assets, retrieve instrument ids
-    assets = [ asset for asset in my_acc.positions()['results']]
-    instr_ids = [ instr['instrument'].split("/")[-2] for instr in assets ]
-    quantity = [ asset['quantity'] for asset in my_acc.positions()['results']]
-    
-    # key, value => instr_id, quantity
-    instrs_quantity = dict(zip(instr_ids, quantity))
-    # remove any sold assets, quantity = 0.0000
-    instrs_quantity = {k: v for k, v in instrs_quantity.items() if v != '0.0000'}
-    
-    # generate portfolio information - stocks, equity, credentials
-    port_stocks = get_port_info(my_acc, instrs_quantity)
-    avail_cash = float(my_acc.portfolios()['withdrawable_amount']) + float(my_acc.portfolios()['unwithdrawable_deposits'])
-    print("avail: ", avail_cash)
-    # create port dict 
-    portfolio = {}
-    portfolio['positions'] = []
-    portfolio['email'] = email
-    portfolio['password'] = password
-    portfolio['equity'] = round(float(my_acc.portfolios()['equity']),2)
-    portfolio['avail_cash'] = avail_cash
-    
-    for stock in port_stocks:
-        # stock object must be serialized as json has limited type system
-        portfolio['positions'].append(json.dumps(stock.__dict__))
+    # check user entry
+    if(db.users.find_one({"email": email})):
+        return True
 
-    # upload port to acc.json         
-    with open("acc.json", "w") as f:
-        json.dump(portfolio, f)
-    
-    return True
+    # new user: insert portfolio
+    portfolio = r.account.build_holdings() # {'TSLA': {'x':'y', 'z':'a',...}, 'AAPL'..}
+    uid = db.users.insert_one({"portfolio":portfolio}).inserted_id
 
-def compare_ports(positions):
+    # new user: insert equity, cash and email
+    profile = r.account.build_user_profile()
+    db.users.find_one_and_update({"_id":uid}, { "$set" : {"equity": profile["equity"]} })
+    db.users.find_one_and_update({"_id":uid}, {"$set": {"cash": profile["cash"]} })
+    db.users.find_one_and_update({"_id":uid}, {"$set": {"email": email} })
+    
+    return True 
+
+def get_acc_info(email):
+    return db.users.find_one({"email": email}, {"_id": 0})
+
+def logout(email):
+    db.users.delete_one({"email": email})
+
+def compare_ports(portfolio):
     '''Retrieve priorities for VTI, VXUS, and VCIT purchases by comparing current portfolio'''
     pprint.pprint('Determine index stock to purchase...')
     priority = {}
 
-    for stock in positions:
-        # Convert Str object to Python dict
-        stockObj = ast.literal_eval(stock)
-        if stockObj['symbol'] == 'VTI' and stockObj['percentage'] < .35:
-            vti_diff = .35 - stockObj['percentage']
-            priority.update({'VTI':vti_diff})
+    for ticker in portfolio:
+        tick_perct = float(portfolio[ticker]["percentage"])
+        if ticker == "VTI" and tick_perct < 35:
+            vti_diff = 35 - tick_perct
+            priority.update({"VTI":vti_diff})
 
-        elif stockObj['symbol'] == 'VXUS' and stockObj['percentage'] < .35:
-            vxus_diff = .35 - stockObj['percentage']
-            priority.update({'VXUS':vxus_diff})
+        elif ticker == "VXUS" and tick_perct < 35:
+            vxus_diff = 35 - tick_perct
+            priority.update({"VXUS":vxus_diff})
 
-        elif stockObj['symbol'] == 'VCIT' and stockObj['percentage'] < .10:
-            vcit_diff = .10 - stockObj['percentage']
-            priority.update({'VCIT':vcit_diff})
+        elif ticker == "VCIT" and tick_perct < 10:
+            vcit_diff = 10 - tick_perct
+            priority.update({"VCIT":vcit_diff})
         
     # return symbols by highest priority in desc order
     return sorted(priority, key=priority.get, reverse=True)
     
-def purchase_quantity(avail_cash, ask_price):
+def purchase_quantity(avail_cash, ask_price, max_amt):
     '''Based on available cash, find max purchase amount with current asking price'''
     quantity, total_cost = 0, 0 
     while avail_cash > total_cost:
         quantity = quantity + 1
         total_cost = quantity * ask_price
+        if(quantity == max_amt + 1):
+            break
+    
     return (quantity - 1)
 
-def rebalance(file='acc.json'):
-    '''Rebalance portfolio by purchasing VIT, VXUS and/or VCIT'''
-    # read from json file
-    with open(file) as f:
-        acc = json.load(f)
-   
-    avail_cash = acc['avail_cash']
-    priorities = compare_ports(acc['positions'])
-    my_acc = Robinhood()
-    my_acc.login(acc['email'], acc['password'])
+def single_order(sticker, quantity):
+    r.order_buy_market(sticker, quantity)
+    return True
 
+def rebalance(email, password, max_amt):
+    '''Rebalance portfolio by purchasing VIT, VXUS and/or VCIT'''
+
+    # 1. Get portfolio
+    portfolio = db.users.find_one({"email":email})["portfolio"]
+    cash = float(db.users.find_one({"email":email})["cash"])
+    
+    # 2. Get priorities
+    priorities = compare_ports(portfolio)
+
+    # 3. Purchase stocks
     purchases = []
-    # purchase as many stock shares per priorities
+    
+    # 4. Authenticate Robinhood
+    r.authentication.login(email, password)
+
     for sticker in priorities:
-        ask_price = float(my_acc.quote_data(sticker)['ask_price'])
-        if  avail_cash > ask_price:
-            quantity = purchase_quantity(avail_cash, ask_price)
-            # retrieve correct stock information
-            stock_instrument = my_acc.instruments(sticker)[0]
-            my_acc.place_buy_order(stock_instrument, quantity, ask_price)
+        ask_price = float(r.get_stock_quote_by_symbol(sticker)["ask_price"])
+        if  cash > ask_price:
+            quantity = purchase_quantity(cash, ask_price, max_amt)
+            # retrieve stock information
+            stock_instrument = r.get_instruments_by_symbols(sticker)[0]
+
+            # WARNING - UNCOMMENT BELOW TO EXECUTE ORDERS IMMEDIATELY!
+            # r.orders.order_buy_market(sticker, quantity)
+
             bought = {}
-            bought['name'] = stock_instrument['simple_name'] 
+            bought['name'] = stock_instrument['name'] 
             bought['quantity'] = quantity
             bought['price'] = ask_price
+            bought['total_price'] = quantity * ask_price
             purchases.append(bought)
-            #update avail cash after stock purchase(s)
-            avail_cash = round(avail_cash - (ask_price*quantity), 2)
+            
+            #update cash after stock purchase(s) 
+            cash = round(cash - (ask_price*quantity), 2)
+    
+    return purchases
 
-    return purchases, acc['email']
+   
